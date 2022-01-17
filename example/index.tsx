0810b2235f44bdccfa3ef05131acec51510f0676
Quentin GRISEL <grisel@doxense.com>
2022-01-17 17:26:33 +0100

    fix (example): Display existing component
    Change the component displayed in the example page

--- example/index.tsx
+++ example/index.tsx
@@ -1,12 +1,12 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../.';
+import { Chip } from '../src/chip';
 
 const App = () => {
   return (
     <div>
-      <Thing />
+      <Chip text="Example" />
     </div>
   );
 };
